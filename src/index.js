8d977abca073bd7cf3c3ede707640827c99688ab
Miluska Pajuelo <miluskapajuelo@gmail.com>
2021-01-15 16:11:46 -0500

    javascript al 50%

--- src/index.js
+++ src/index.js
@@ -16,11 +16,33 @@ function press1(){
 
     document.getElementById("incorrecto").classList.add("display");
     document.getElementById("incorrecto").classList.remove("hide");
+    console.log(typeof(validacion1)) ;
+    var a = validacion1.split("").reverse().join("");
+    console.log(a[0]);
+    console.log(a[a.length-1]);
+    console.log(a.length);
+    var c = Array.from(a);
+    console.log(c);
+    
+    for(var i=0; i<c.length; i++){
+      if(i%2==0){
+        var d = c[i]*2;
+        if(d >10){
+          console.log(d);
+        }
+        else{
+          console.log("hola mundo")
+        }
+      }
+      
+    }
   }
-else{
+  else{
   alert("Debe colocar el dni para iniciar")
 }
-}
+
+    
+  }
 
 
 var retorna;
@@ -35,3 +57,15 @@ function press2(){
     document.getElementById("inicio").classList.add("display");
     document.getElementById("inicio").classList.remove("hide");
 }
+
+var card = document.getElementById("card");
+var hola = [];
+var NameValues=[];
+card.addEventListener("keypress", function soloNumeros(e){
+      var key = card.event ? e.which : e.keyCode;
+      if (key < 48 || key > 57) {
+        e.preventDefault();
+       }
+    })
+
+
